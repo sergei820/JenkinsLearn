42f39d823be491bd282d62415ba44d4fce9549d3
Sergei_Kamendrovskii <se25051990@gmail.com>
2023-02-17 11:20:24 +0400

    file added

--- Hello.java
+++ Hello.java
@@ -2,9 +2,9 @@ public class Hello {
 
     public static void main(String[] args) {
 
-        for(int i = 0; i < 10; i++) {
-            System.out.println("Hello, Jenkins!");
+        for(int i = 0; i < 3; i++) {
+            System.out.println("Hello, Jenkins " + (i+1));
+            System.out.println("readme.txt added");
         }
-
     }
 }
